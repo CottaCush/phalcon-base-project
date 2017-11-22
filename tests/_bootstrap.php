95b3868a17808f5f5af06f9047d146db7b97c5e6
Adeyemi Olaoye <yemi@cottacush.com>
2017-11-22 15:29:54 +0100

    Disable Oauth by default and remove ant build from bootstrap

--- tests/_bootstrap.php
+++ tests/_bootstrap.php
@@ -2,6 +2,4 @@
 
 include_once dirname(__FILE__) . '/../public/index.php';
 
-exec('DB_USERNAME=' . getenv('DB_USERNAME') . ' DB_PASSWORD= DB_NAME=' . getenv('DB_NAME') . ' DB_HOST=' . getenv('DB_HOST') . ' CLIENT_SECRET=' . getenv('CLIENT_SECRET') . ' CLIENT_ID=' . getenv('CLIENT_ID') . ' ant migrations');
-
 include_once '_support/CommonTests.php';
